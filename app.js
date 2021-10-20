afc35e5ba948d9458abc7922937d1f99dd781959
Dan Possehl <danielbpossehl@gmail.com>
2021-10-20 10:01:43 -0600

    worked on functions

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-const name = "john";
-const lstName = "jordan";
+const wallheight = 80;
+
+const height = calculate(100);
+const widthc = calculate(wallheight);
+
+const dimensions = [width, height];
+console.log(dimensions);
+
+function calculate(value) {
+  console.log("the value in cm is: " + value * 2.54 + "cm");
+  return 45;
+}
+
+calculate();
