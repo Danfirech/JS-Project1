9edcfcf058d21174c7ecf3242d54dd5b2839c353
Dan Possehl <danielbpossehl@gmail.com>
2021-10-25 09:32:23 -0600

    while loop

--- app.js
+++ app.js
@@ -1,38 +1,7 @@
-const dice = 0;
+let amount = 10;
 
+while (amount > 0) {
+  console.log("i have " + amount + " dollars and im going to the mall");
 
-switch(dice) {
-  case 1:
-    console.log('you got one')
-    break;
-  
-    case 2:
-    console.log('you got two')
-    break;
-    
-  case 3:
-    console.log('you got three')
-    break;
-
-  default:
-  console.log('you did not roll the dice')
-    
-
-}
-
-
-
-
-
-// esle if 
-if (dice === 1) {
-  console.log("you got one");
-}
-
- else if (dice === 2) {
-  console.log("you got two");
-}
-
-else (dice < 1 || dice > 6) {
-  console.log("you did not roll the dice");
+  amount--;
 }
