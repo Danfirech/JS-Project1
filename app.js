07fc07e5ce6182bfd6fd48eec20895ed216d20f1
Dan Possehl <danielbpossehl@gmail.com>
2021-10-29 10:01:04 -0600

    Global Scope

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
-const gas = [20, 40, 100, 30];
+const globalNumber = 5;
 
-const food = [10, 40, 50];
-
-function calculateTotal(arr) {
-  let total = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    total += arr[i];
-  }
-  if (total > 100) {
-    console.log("over spending");
-  } else console.log("you are within your budget");
+function add(num1, num2) {
+  const result = num1 + num2 + globalNumber;
+  return result;
 }
 
-const gasTotal = calculateTotal(food);
-const foodTotal = calculateTotal(gas);
-const randomTotal = calculateTotal();
-
-console.log({
-  gas: gasTotal,
-  food: foodTotal,
-  randomTotal: randomTotal,
-});
+console.log(add(3, 4));
