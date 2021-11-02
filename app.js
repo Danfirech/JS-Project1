3179a4febc5776aa54aeb1a13daea3738560a499
Dan Possehl <danielbpossehl@gmail.com>
2021-11-02 09:40:29 -0600

    arrays

--- app.js
+++ app.js
@@ -1,26 +1,5 @@
-function morning(name) {
-  return `Good Morning ${name.toUpperCase()}`;
+const numbers = [0, 1, 2, 3, 4];
+
+for (let i = 0; i < numbers.length; i++) {
+  cosnoel.log(numbers[i]);
 }
-
-function afternoon(name) {
-  return `Good Morning ${name.toUpperCase()}`;
-}
-
-function greet(name, cb) {
-  const myName = "john";
-  console.log(`${cb(name)}, my name is ${myName}`);
-}
-
-greet("bobo", morning);
-
-greet("peter", afternoon);
-
-// function greetAfternoon(name) {
-//   const myName = "john";
-//   console.log(`good afternoon ${name}, my name is ${myName}`);
-// }
-
-// function greetMorning(name) {
-//   const myName = "john";
-//   console.log(`good morning ${name}, my name is ${myName}`);
-// }
