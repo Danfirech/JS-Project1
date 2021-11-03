d9b6b5cfac5f22cb6fc29dcfb55b7ca3e2124658
Dan Possehl <danielbpossehl@gmail.com>
2021-11-03 09:22:52 -0600

    filter

--- app.js
+++ app.js
@@ -2,22 +2,15 @@ const people = [
   { name: "bob", age: 20, position: "developer" },
   { name: "peter", age: 25, position: "designer" },
   { name: "susy", age: 20, position: "the boss" },
+  { name: "anna", age: 35, position: "the boss" },
 ];
 
-const ages = people.map(function (person) {
-  return person.age + 20;
+const youngPeople = people.filter(function (person) {
+  return person.age <= 25;
 });
 
-const newPeople = people.map(function (person) {
-  return { firstname: person.name.toUpperCase(), oldAge: person.age + 20 };
+const developers = people.filter(function (person) {
+  return person.position === "developer";
 });
 
-console.log(newPeople);
-
-const names = people.map(function (person) {
-  return `<h1>${person.name}</h1>`;
-});
-
-document.body.innerHTML = names.join("");
-
-console.log(names);
+console.log(developers);
