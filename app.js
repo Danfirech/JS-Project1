2e1a830a82b78dd80961f98a1bd80ab446559b9e
Dan Possehl <danielbpossehl@gmail.com>
2021-11-03 09:36:45 -0600

    find

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const people = [
-  { name: "bob", age: 20, position: "developer" },
-  { name: "peter", age: 25, position: "designer" },
-  { name: "susy", age: 20, position: "the boss" },
-  { name: "anna", age: 35, position: "the boss" },
+  { name: "bob", age: 20, position: "developer", id: 1 },
+  { name: "peter", age: 25, position: "designer", id: 2 },
+  { name: "susy", age: 20, position: "the boss", id: 3 },
+  { name: "anna", age: 35, position: "the boss", id: 4 },
 ];
 
-const youngPeople = people.filter(function (person) {
-  return person.age <= 25;
+const names = ["bob", "peter", "susy"];
+const person = people.find(function (person) {
+  return person.id === 3;
 });
 
-const developers = people.filter(function (person) {
-  return person.position === "developer";
+console.log(
+  names.find(function (name) {
+    return name === "bob";
+  })
+);
+console.log(person);
+
+const person2 = people.filter(function (person) {
+  return person.id === 3;
 });
 
-console.log(developers);
+console.log(person2);
