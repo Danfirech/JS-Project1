b29141de13452a1bcbeb60e31114a2f9f0a8656b
Dan Possehl <danielbpossehl@gmail.com>
2021-11-08 09:45:35 -0700

    Date method

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
-const number = 4.56789;
+// Date
 
-const result = Math.floor(Math.random() * 10);
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+];
 
-console.log(result);
+const days = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+const date = new Date();
+const month = date.getMonth();
+console.log(months[month]);
+
+const day = date.getDay();
+console.log(days[day]);
+
+console.log(date.getDate());
+console.log(date.getFullYear());
+
+const sentance = `${days[day]}, ${date.getDate()} ${
+  months[month]
+} ${date.getFullYear()}`;
+
+console.log(sentance);
