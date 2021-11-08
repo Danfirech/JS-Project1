fc15a53330a0c5833015988c29033e0234bbfdb9
Dan Possehl <danielbpossehl@gmail.com>
2021-11-08 09:32:19 -0700

    Math.random

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
-const people = [
-  { name: "bob", age: 20, position: "developer", id: 1 },
-  { name: "peter", age: 25, position: "designer", id: 2 },
-  { name: "susy", age: 20, position: "the boss", id: 3 },
-  { name: "anna", age: 35, position: "the boss", id: 4 },
-];
+const number = 4.56789;
 
-const names = ["bob", "peter", "susy"];
-const person = people.find(function (person) {
-  return person.id === 3;
-});
+const result = Math.floor(Math.random() * 10);
 
-console.log(
-  names.find(function (name) {
-    return name === "bob";
-  })
-);
-console.log(person);
-
-const person2 = people.filter(function (person) {
-  return person.id === 3;
-});
-
-console.log(person2);
+console.log(result);
