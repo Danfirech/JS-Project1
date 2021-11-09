fbaad9d212e0c559b29aba43b093c1422e691890
Dan Possehl <danielbpossehl@gmail.com>
2021-11-09 09:58:12 -0700

    Navigate the Dom

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
 const result = document.querySelector("#result");
 
-result.style.backgroundColor = "blue";
-
-const item = document.querySelectorAll(".special");
-
-const lastItem = document.querySelector("li:last-child");
-
-const list = document.querySelectorAll(".special");
-console.log(list);
-
-list.forEach(function (item) {
-  console.log(item);
-  item.style.color = "yellow";
-});
+console.log(result.firstChild);
+console.log(result.lastChild);
