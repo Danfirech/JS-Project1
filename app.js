cea2de006a885aa90c34e2ad189468aadf8faa69
Dan Possehl <danielbpossehl@gmail.com>
2021-11-18 09:49:25 -0700

    mouse events

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-// select element
-// addEventListener()
-// what event, what to do
+const heading = document.querySelector("h1");
 
 const btn = document.querySelector(".btn");
-const heading = document.querySelector("h2");
 
-function changeColors() {
-  let hasClass = heading.classList.contains("red");
-  if (hasClass) {
-    heading.classList.remove("red");
-  } else {
-    heading.classList.add("red");
-  }
-}
+btn.addEventListener("click", function () {
+  console.log("you clicked me");
+});
 
-btn.addEventListener("click", changeColors);
+btn.addEventListener("mousedown", function () {
+  console.log("down");
+});
+
+btn.addEventListener("mouseup", function () {
+  console.log("up");
+});
+
+heading.addEventListener("mouseenter", function () {
+  heading.classList.add("blue");
+});
