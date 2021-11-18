81fa3506d8ef533b30280fa241ba907e4259889e
Dan Possehl <danielbpossehl@gmail.com>
2021-11-18 09:39:50 -0700

    add event listner

--- app.js
+++ app.js
@@ -5,6 +5,13 @@
 const btn = document.querySelector(".btn");
 const heading = document.querySelector("h2");
 
-btn.addEventListener("click", function () {
-  heading.classList.add("red");
-});
+function changeColors() {
+  let hasClass = heading.classList.contains("red");
+  if (hasClass) {
+    heading.classList.remove("red");
+  } else {
+    heading.classList.add("red");
+  }
+}
+
+btn.addEventListener("click", changeColors);
